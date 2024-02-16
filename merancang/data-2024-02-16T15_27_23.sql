101f2498ab8a5cf7ec68ca2e264eecdb8bdda3ed
mzaini30 <muhzaini30@gmail.com>
2024-02-16 15:55:38 +0800

    init

---
+++ merancang/data-2024-02-16T15_27_23.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE data.barang
+(
+  id         TEXT    NOT NULL,
+  nama       TEXT    NULL    ,
+  harga_beli DECIMAL NULL    ,
+  harga_jual DECIMAL NULL    ,
+  banyak     INT     NULL    ,
+  PRIMARY KEY (id)
+);
--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,22 @@
   import Router from "svelte-spa-router";
   import Gerbang from "./kunci/src/Gerbang.svelte";
   import data from "../android.json";
+  import alasql from "alasql";
+
+  alasql(/* sql */ `
+  create localStorage database if not exists data;
+  attach localStorage database data;
+  
+CREATE TABLE if not exists data.barang
+(
+  id         text     NOT NULL,
+  nama       TEXT    NULL    ,
+  harga_beli DECIMAL NULL    ,
+  harga_jual DECIMAL NULL    ,
+  banyak     INT     NULL    ,
+  PRIMARY KEY (id)
+);
+  `);
 
   const routes = {};
 </script>
